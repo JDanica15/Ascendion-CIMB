a75b843369e01f51324486542c25ca27c60c53c6
James Dollente <jamespogitodaw@gmail.com>
2025-02-21 13:31:17 +0800

    fix styling of articlePreview

--- AscendionExam/app/components/ArticlePreview/articlePreview.style.ts
+++ AscendionExam/app/components/ArticlePreview/articlePreview.style.ts
@@ -4,20 +4,25 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#fff",
         padding: 10,
+        flex: 1,
+        width: '100%',
+        height: '100%',
     },
     image: {
         width: "100%",
-        height: 150,
+        height: '100%',
+        maxHeight: 300,
         borderTopRightRadius: 10,
         borderTopLeftRadius: 10
     },
     summary: {
-        marginVertical: 16,
+        marginVertical: 20,
         fontSize: 16,
         color: "#333",
+        fontWeight: '500'
     },
     button: {
-        backgroundColor: "red",
+        backgroundColor: "#cc0100",
         padding: 10,
         borderRadius: 5,
         alignItems: "center",
